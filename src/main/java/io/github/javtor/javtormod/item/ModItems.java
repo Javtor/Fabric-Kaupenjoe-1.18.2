736f629e3107be87549ec3c90d460b873e874d8f
Javier Torres <javierandrestorresreyes@gmail.com>
2022-05-31 15:21:48 -0500

    Adding items

---
+++ src/main/java/io/github/javtor/javtormod/item/ModItems.java
@@ -0,0 +1,29 @@
+package io.github.javtor.javtormod.item;
+
+import io.github.javtor.javtormod.JavtorMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+
+    public static final Item JAVTOR_INGOT = registerItem("javtor_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(JavtorMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        JavtorMod.LOGGER.info("Registering Mod Items for "+JavtorMod.MOD_ID);
+    }
+}
